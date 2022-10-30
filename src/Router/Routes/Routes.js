62553ac6e1109a471f31853c84aa16ed368e09ca
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-31 01:42:16 +0600

    project setup

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,17 @@
+import Main from "../../Layout/Main";
+import Home from "../../Pages/Home/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
