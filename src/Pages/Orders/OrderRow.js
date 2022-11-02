dcb6dbf95f163cd5d3fd664a0b3d8950d785501e
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-02 19:09:25 +0600

    update operation implemented

--- src/Pages/Orders/OrderRow.js
+++ src/Pages/Orders/OrderRow.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-const OrderRow = ({ order, handleDelete }) => {
-    const { _id, serviceName, customerName, phone, price, service } = order;
+const OrderRow = ({ order, handleDelete, handleStatusUpdate }) => {
+    const { _id, serviceName, customerName, phone, price, service, status } = order;
 
     const [orderService, setOderService] = useState({});
 
@@ -41,7 +41,7 @@ const OrderRow = ({ order, handleDelete }) => {
             </td>
             <td>Purple</td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                <button onClick={() => handleStatusUpdate(_id)} className="btn btn-ghost btn-xs">{status ? status : 'Pending'}</button>
             </th>
         </tr>
     );
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -29,6 +29,28 @@ const Orders = () => {
                     }
                 })
         }
+    };
+
+    const handleStatusUpdate = id => {
+        fetch(`http://localhost:5000/orders/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'Approved' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    const remaining = orders.filter(odr => odr._id !== id);
+                    const approving = orders.find(odr => odr._id === id)
+                    approving.status = 'Approved'
+
+                    const newOrders = [approving, ...remaining];
+                    setOrders(newOrders)
+                }
+            })
     }
 
     return (
@@ -51,6 +73,7 @@ const Orders = () => {
                                 key={order._id}
                                 order={order}
                                 handleDelete={handleDelete}
+                                handleStatusUpdate={handleStatusUpdate}
                             ></OrderRow>)
                         }
                     </tbody>
