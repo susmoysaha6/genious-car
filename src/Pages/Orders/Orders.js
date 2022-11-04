d4fbe69ed643b5ed7493cb7686b43ae466cfd7bb
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-04 15:16:19 +0600

    jwt applied for post delete and update orders

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -9,7 +9,9 @@ const Orders = () => {
 
     useEffect(() => {
         fetch(`http://localhost:5000/orders?email=${user?.email}`, {
-            headers: { authorization: `Bearer ${localStorage.getItem('genius-token')}` }
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('genius-token')}`
+            }
         })
             .then(res => {
                 if (res.status === 401 || res.status === 403) {
@@ -24,7 +26,10 @@ const Orders = () => {
         const proceed = window.confirm('Are you sure, you want to cancel this order');
         if (proceed) {
             fetch(`http://localhost:5000/orders/${id}`, {
-                method: 'DELETE'
+                method: 'DELETE',
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('genius-token')}`
+                }
             })
                 .then(res => res.json())
                 .then(data => {
@@ -42,7 +47,8 @@ const Orders = () => {
         fetch(`http://localhost:5000/orders/${id}`, {
             method: 'PATCH',
             headers: {
-                'content-type': 'application/json'
+                'content-type': 'application/json',
+                authorization: `Bearer ${localStorage.getItem('genius-token')}`
             },
             body: JSON.stringify({ status: 'Approved' })
         })
