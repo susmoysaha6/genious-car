74df7a13e92786f95f1573cfd11875c596293543
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-11 22:02:22 +0600

    search field add

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, SetServices] = useState([]);
+    const [isAsc, serISAsc] = useState(true);
+    const [search, setSearch] = useState('')
+    const searchRef = useRef();
     useEffect(() => {
-        fetch('https://genius-car-server-gilt.vercel.app/services')
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => SetServices(data))
-    }, [])
+    }, [isAsc, search])
+
+
+    const handleSearch = () => {
+        setSearch(searchRef.current.value);
+    }
+
     return (
         <div>
             <div className='text-center'>
                 <p className="text-2xl font-bold text-orange-600">Service</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+                <input className='input input-sm' ref={searchRef} type="text" name="" id="" />
+                <button onClick={handleSearch}>Search</button>
+                <button className='btn btn-ghost' onClick={() => serISAsc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10'>
                 {
